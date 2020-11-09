548aed938295cf857cdb8950c490d1a373c2c931
xuzhangmin <1921936779@qq.com>
2020-11-09 19:40:11 +0800

    第一次提交

---
+++ setTimeout.js
@@ -0,0 +1,11 @@
+ function fun () {
+        var a = 0;
+        function add () {
+            a++;
+            console.log(a);
+        }
+        add();
+     }
+ 
+ var test=fun();
+console.log(test);
